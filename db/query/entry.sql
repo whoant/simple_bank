9659b9011dd632b0374afc33630378b0adca627e
whoant <vovanhoangtuan4.2@gmail.com>
2023-01-29 02:22:52 +0700

    Add entry and trasfer method

---
+++ db/query/entry.sql
@@ -0,0 +1,12 @@
+-- name: CreateEntry :one
+INSERT INTO entries (
+    account_id,
+    amount
+) VALUES (
+    $1, $2
+)
+RETURNING *;
+
+-- name: GetEntry :one
+SELECT * FROM entries
+WHERE id = $1 LIMIT 1;
