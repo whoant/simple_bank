054e1a8953a0f5ad35c399ad4c5562fb126458f1
whoant <vovanhoangtuan4.2@gmail.com>
2023-01-28 23:30:18 +0700

    Init code

---
+++ db/sqlc/main_test.go
@@ -0,0 +1,28 @@
+package db
+
+import (
+	"database/sql"
+	"log"
+	"os"
+	"testing"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	dbDriver = "postgres"
+	dbSource = "postgresql://postgres:Vovanhoangtuan1@localhost:5433/simple_bank?sslmode=disable"
+)
+
+var testQueries *Queries
+
+func TestMain(m *testing.M) {
+	conn, err := sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatalf("cannot connect to db : %v", err)
+	}
+
+	testQueries = New(conn)
+
+	os.Exit(m.Run())
+}
