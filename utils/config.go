4e00fed5be16c13e2a885c4dc2a8fafce4a23673
whoant <vovanhoangtuan4.2@gmail.com>
2023-02-11 18:54:02 +0700

    add new config

--- utils/config.go
+++ utils/config.go
@@ -1,15 +1,19 @@
 package utils
 
 import (
+	"time"
+
 	"github.com/spf13/viper"
 )
 
 // Config stores all configuration of the application.
 // The values are read by viper from a config file or environment variables.
 type Config struct {
-	DBDriver      string `mapstructure:"DB_DRIVER"`
-	DBSource      string `mapstructure:"DB_SOURCE"`
-	ServerAddress string `mapstructure:"SERVER_ADDRESS"`
+	DBDriver            string        `mapstructure:"DB_DRIVER"`
+	DBSource            string        `mapstructure:"DB_SOURCE"`
+	ServerAddress       string        `mapstructure:"SERVER_ADDRESS"`
+	TokenSymmetricKey   string        `mapstructure:"TOKEN_SYMMETRIC_KEY"`
+	AccessTokenDuration time.Duration `mapstructure:"ACCESS_TOKEN_DURATION"`
 }
 
 func LoadConfig(path string) (config Config, err error) {
@@ -17,7 +21,7 @@ func LoadConfig(path string) (config Config, err error) {
 	viper.SetConfigName("app")
 	viper.SetConfigType("env")
 	viper.AutomaticEnv()
-	
+
 	err = viper.ReadInConfig()
 	if err != nil {
 		return
