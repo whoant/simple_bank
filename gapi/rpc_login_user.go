47f8ca30cf5c87152fd822bc8923d20a555883fd
whoant <vovanhoangtuan4.2@gmail.com>
2023-02-19 02:30:47 +0700

    add rpc_login_user

---
+++ gapi/rpc_login_user.go
@@ -0,0 +1,63 @@
+package gapi
+
+import (
+	"context"
+	"database/sql"
+
+	db "github.com/whoant/simple_bank/db/sqlc"
+	"github.com/whoant/simple_bank/pb"
+	"github.com/whoant/simple_bank/utils"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/types/known/timestamppb"
+)
+
+func (server *Server) LoginUser(ctx context.Context, req *pb.LoginUserRequest) (*pb.LoginUserResponse, error) {
+	user, err := server.store.GetUser(ctx, req.GetUsername())
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, status.Errorf(codes.NotFound, "user not found")
+		}
+		return nil, status.Errorf(codes.Internal, "failed to find user : %s", err)
+	}
+
+	err = utils.CheckPassword(req.GetPassword(), user.HashedPassword)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "incorrect password")
+	}
+
+	accessToken, accessTokenPayload, err := server.tokenMaker.CreateToken(user.Username, server.config.AccessTokenDuration)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "failed to create accessToken: %s", err)
+	}
+
+	refreshToken, refreshTokenPayload, err := server.tokenMaker.CreateToken(user.Username, server.config.RefreshTokenDuration)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "failed to create refresh accessToken: %s", err)
+	}
+
+	session, err := server.store.CreateSession(ctx, db.CreateSessionParams{
+		ID:           refreshTokenPayload.ID,
+		Username:     user.Username,
+		RefreshToken: refreshToken,
+		UserAgent:    "",
+		ClientIp:     "",
+		IsBlocked:    false,
+		ExpiredAt:    refreshTokenPayload.ExpiredAt,
+	})
+
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "failed to create session: %s", err)
+	}
+
+	resp := &pb.LoginUserResponse{
+		User:                  convertUser(&user),
+		SessionId:             session.ID.String(),
+		AccessToken:           accessToken,
+		AccessTokenExpiredAt:  timestamppb.New(accessTokenPayload.ExpiredAt),
+		RefreshToken:          refreshToken,
+		RefreshTokenExpiredAt: timestamppb.New(refreshTokenPayload.ExpiredAt),
+	}
+
+	return resp, nil
+}
