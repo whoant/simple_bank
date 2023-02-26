107f199e311326306d2c59b5194f747a8b0814f7
whoant <vovanhoangtuan4.2@gmail.com>
2023-02-27 01:07:29 +0700

    add validate for grpc gateway

--- gapi/rpc_login_user.go
+++ gapi/rpc_login_user.go
@@ -7,12 +7,26 @@ import (
 	db "github.com/whoant/simple_bank/db/sqlc"
 	"github.com/whoant/simple_bank/pb"
 	"github.com/whoant/simple_bank/utils"
+	"github.com/whoant/simple_bank/val"
+	"google.golang.org/genproto/googleapis/rpc/errdetails"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 func (server *Server) LoginUser(ctx context.Context, req *pb.LoginUserRequest) (*pb.LoginUserResponse, error) {
+	violations := validateLoginUserRequest(req)
+	if violations != nil {
+		badRequest := &errdetails.BadRequest{FieldViolations: violations}
+		statusInvalid := status.New(codes.InvalidArgument, "invalid argument")
+		statusDetails, err := statusInvalid.WithDetails(badRequest)
+		if err != nil {
+			return nil, statusInvalid.Err()
+		}
+
+		return nil, statusDetails.Err()
+	}
+
 	user, err := server.store.GetUser(ctx, req.GetUsername())
 	if err != nil {
 		if err == sql.ErrNoRows {
@@ -62,4 +76,16 @@ func (server *Server) LoginUser(ctx context.Context, req *pb.LoginUserRequest) (
 	}
 
 	return resp, nil
+}
+
+func validateLoginUserRequest(req *pb.LoginUserRequest) (violations []*errdetails.BadRequest_FieldViolation) {
+	if err := val.ValidateUsername(req.GetUsername()); err != nil {
+		violations = append(violations, fieldViolation("username", err))
+	}
+
+	if err := val.ValidatePassword(req.GetPassword()); err != nil {
+		violations = append(violations, fieldViolation("password", err))
+	}
+
+	return violations
 }
