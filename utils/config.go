79d5a0bc3509d60150125d777edafe02af0d0d77
whoant <vovanhoangtuan4.2@gmail.com>
2023-04-09 18:55:30 +0700

    add REDIS_ADDRESS config

--- utils/config.go
+++ utils/config.go
@@ -18,6 +18,7 @@ type Config struct {
 	AccessTokenDuration  time.Duration `mapstructure:"ACCESS_TOKEN_DURATION"`
 	RefreshTokenDuration time.Duration `mapstructure:"REFRESH_TOKEN_DURATION"`
 	Environment          string        `mapstructure:"ENVIRONMENT"`
+	RedisAddress         string        `mapstructure:"REDIS_ADDRESS"`
 }
 
 func LoadConfig(path string) (config Config, err error) {
